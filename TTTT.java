50e3d024d1204affab9c45ec556ef6e4ec4423e7
OH <dhcksehf@gmail.com>
2022-12-25 17:56:19 +0900

    practice

---
+++ TTTT.java
@@ -0,0 +1,13 @@
+
+public class TTTT {
+
+	public static void main(String[] args) {
+		int counter = 1;
+				
+			for (   	 ; counter <= 10; counter++ ) {
+				System.out.println(counter);
+				
+			} //for
+			System.out.println("counter: " + counter);
+	}
+}
